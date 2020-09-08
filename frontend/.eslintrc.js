4272547e94b2278b3bf51096240d602d5282a049
김동욱 <gustkdxo007@gmail.com>
2020-09-08 15:31:04 +0900

    :pencil: Add Git Rull & eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    quotes: ["error", "single"],
+    camelcase: "off",
+    "@typescript-eslint/camelcase": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
