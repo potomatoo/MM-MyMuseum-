26fbd17ab3756e6f7179937b2b0eaa1345676ea8
김동욱 <gustkdxo007@gmail.com>
2020-09-09 23:54:02 +0900

    :sparkles: Login page prototype

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,16 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <router-link :to="{ name: 'Home' }">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </router-link>
 
         <v-img
           alt="Vuetify Name"
@@ -23,35 +25,26 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn :to="{ name: 'Login' }" text>
+        <span class="mr-2">Login</span>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Vue, Component } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: "App",
+import Home from "@/views/Home.vue";
 
+@Component({
   components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-});
+    Home
+  }
+})
+export default class App extends Vue {}
 </script>
---
+++ frontend/src/views/accounts/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="width: 50%" class="mx-auto mt-15">
+    <h1 class="my-3">Sign in</h1>
+    <v-form>
+      <v-text-field
+        v-model="userEmail"
+        :rules="emailRules"
+        solo
+        label="User Email"
+        autofocus
+      ></v-text-field>
+      <v-text-field
+        v-model="userPassword"
+        :rules="passwordRules"
+        solo
+        label="Password"
+        type="password"
+      ></v-text-field>
+      <v-btn color="indigo" width="100%" dark large>로그인</v-btn>
+    </v-form>
+    <v-divider class="my-4" />
+    <v-spacer />
+    <p>계정이 없으신가요?</p>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class LoginView extends Vue {
+  userEmail = "";
+  userPassword = "";
+  emailRules = [
+    v => !!v || "이메일을 입력하세요",
+    v => /.+@.+\..+/.test(v) || "올바른 이메일 형식으로 입력하세요"
+  ];
+  passwordRules = [
+    v => !!v || "비밀번호를 입력하세요",
+    v => v.length > 8 || "비밀번호는 8자 이상 입력해주세요"
+  ];
+}
+</script>
+
+<style></style>
