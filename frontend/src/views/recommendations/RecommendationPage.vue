172648c230dd613a6d1ff9059023d1264168249a
김동욱 <gustkdxo007@gmail.com>
2020-09-25 10:42:05 +0900

    :sparkles: Add recommendation page proto type

--- frontend/src/views/recommendations/RecommendationPage.vue
+++ frontend/src/views/recommendations/RecommendationPage.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="recommendation-page">
-    <h1>니가 좋아할만한 작품</h1>
-    <vue-slick-carousel class="slick" v-bind="settings">
-      <div v-for="(article, i) in articles" :key="i">
-        <img
-          class="recommendation-img"
-          :src="require(`@/assets/dummydata/articles/${article.hero}`)"
-          :alt="article.author"
-        />
-      </div>
-    </vue-slick-carousel>
+    <div class="rec-section">
+      <h1 class="rec-title">니가 좋아할만한 작품</h1>
+      <vue-slick-carousel class="slick" v-bind="settings">
+        <div v-for="(article, i) in articles" :key="i">
+          <img
+            class="recommendation-img"
+            :src="require(`@/assets/dummydata/articles/${article.hero}`)"
+            :alt="article.author"
+          />
+        </div>
+      </vue-slick-carousel>
+    </div>
+    <div class="rec-section">
+      <h1 class="rec-title">작가 추천</h1>
+      <vue-slick-carousel class="slick" v-bind="settingsrtl">
+        <div v-for="(article, i) in articles" :key="i">
+          <img
+            class="recommendation-img"
+            :src="require(`@/assets/dummydata/articles/${article.hero}`)"
+            :alt="article.author"
+          />
+        </div>
+      </vue-slick-carousel>
+    </div>
   </div>
 </template>
 
 <script>
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Watch } from "vue-property-decorator";
 import { namespace } from "vuex-class";
 import { Article } from "../../store/ArticleInterface";
 
@@ -33,20 +47,111 @@ export default class RecommendationPage extends Vue {
   @articleModule.State articles;
   @articleModule.Mutation SET_ARTICLE;
 
+  window = {
+    width: 0,
+    height: 0
+  };
+
   settings = {
     arrows: false,
     dots: true,
     infinite: true,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 3000,
-    autoplaySpeed: 2000,
-    cssEase: "linear"
+    speed: 2000,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1399,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1099,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 799,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 499,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
   };
+
+  settingsrtl = {
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+    rtl: true,
+    responsive: [
+      {
+        breakpoint: 1399,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1099,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 799,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 499,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  };
+  // onHover() {}
+  @Watch("window", { deep: true })
+  setDot() {
+    const dots = document.querySelectorAll(".slick-dots");
+    dots.forEach(el => {
+      el.style.bottom = "-40px";
+    });
+  }
+
+  handleResize() {
+    this.window.width = window.innerWidth;
+    this.window.height = window.innerHeight;
+  }
 
   created() {
     this.SET_ARTICLE();
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  }
+
+  mounted() {
+    this.setDot();
+  }
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
   }
 }
 </script>
@@ -57,12 +162,29 @@ export default class RecommendationPage extends Vue {
 }
 
 .recommendation-img {
-  height: 300px;
-  width: 100%;
+  margin: 5px;
+  width: 98%;
+  height: 165px;
+  transition: all ease 1ms;
+}
+
+.recommendation-img:hover {
+  cursor: pointer;
+  box-shadow: 0px 0px 10px 5px;
+  transform: scale(1.1);
 }
 
 .slick {
   width: 100%;
-  height: 300px;
+  height: 165px;
+}
+
+.rec-section {
+  margin-bottom: 50px;
+}
+
+.rec-title {
+  margin-bottom: 10px;
+  color: white;
 }
 </style>
