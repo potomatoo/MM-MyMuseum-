85cfff6c5373e9e5390df9849a31136175ebf7bb
김준호 <tuvmxm@naver.com>
2020-09-27 06:18:57 +0000

    Update settings.py

--- backend/Django/recommend/settings.py
+++ backend/Django/recommend/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'recommend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'j3b205.p.ssafy.io',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'test',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'j3b205.p.ssafy.io',
+    #     'PORT': '3306',
+    # }
 }
 
 
