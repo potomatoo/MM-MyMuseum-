0c9de058d38b62944371bebb2cef860945f7427a
이연재 <duseh73@gmail.com>
2020-09-28 19:11:58 +0900

    :art: change background & design

--- frontend/src/views/mygallery/MygalleryView.vue
+++ frontend/src/views/mygallery/MygalleryView.vue
@@ -1,106 +1,39 @@
 <template>
-  <div>
-    <div class="mygallery">
-      <div class="box">
-        <span style="--i:1;"
-          ><img
-            src="https://lh4.ggpht.com/UuYCUnqvo2EIZhyFHYFVLbkmma_cubVk7SwxOF3lklT6aor5647BXVhEaFB7jg"
-        /></span>
-        <span style="--i:2;"
-          ><img
-            src="https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c"
-        /></span>
-        <span style="--i:3;"
-          ><img
-            src="https://lh4.ggpht.com/zwvR_x7jq9u9Oo1vQ2QrIF5KPNcxM9jWu20F1n_AgX1QvCqLRWgXYTqt5H0"
-        /></span>
-        <span style="--i:4;"
-          ><img
-            src="https://lh6.ggpht.com/rCrxkaW6mPFJ6V2cWCGpmW5SkGo2SrEpB71oX8vWRxOVoiEDdT3BOBiHyJQ"
-        /></span>
-        <span style="--i:5;"
-          ><img
-            src="https://lh3.ggpht.com/MA_gR9Zh_v3ew1n91JoAhjIyt_CprhrLP-qX2J92hKfQaYmMh80fMIjZ8po"
-        /></span>
-        <span style="--i:6;"
-          ><img
-            src="https://lh5.ggpht.com/BukMxmYxe18AYLp-yRd64pXDDIDRWS2YiF8q95zTRRosNUqrXVQllxv4Vwk"
-        /></span>
-        <span style="--i:7;"
-          ><img
-            src="https://lh5.ggpht.com/BukMxmYxe18AYLp-yRd64pXDDIDRWS2YiF8q95zTRRosNUqrXVQllxv4Vwk"
-        /></span>
-        <span style="--i:8;"
-          ><img
-            src="https://lh5.ggpht.com/BukMxmYxe18AYLp-yRd64pXDDIDRWS2YiF8q95zTRRosNUqrXVQllxv4Vwk"
-        /></span>
-      </div>
+  <section id="all">
+    <div id="clicked1">
+      <img
+        class="box"
+        src="https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c"
+        width="100%"
+        height="auto"
+      />
     </div>
-    <detail-artist-arts />
-  </div>
+  </section>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
-import DetailArtistArts from "@/components/detail/DetailArtistArts.vue";
-
-@Component({
-  components: {
-    DetailArtistArts
-  }
-})
+import { Component, Vue } from "vue-property-decorator";
 export default class MygalleryView extends Vue {}
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-.mygallery {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background: #000;
-}
-
-.box {
+#all {
+  width: 100%;
+  height: 100%;
   position: relative;
-  width: 200px;
-  height: 200px;
-  transform-style: preserve-3d;
-  animation: animate 20s linear infinite;
+  background: url("../../assets/mygallery.png") no-repeat;
+  background-size: 100% 100%;
 }
-
-@keyframes animate {
-  0% {
-    transform: perspective(1000px) rotateY(0deg);
-  }
-  100% {
-    transform: perspective(1000px) rotateY(360deg);
-  }
-}
-.box span {
+#clicked1 {
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  transform-origin: center;
-  transform-style: preserve-3d;
-  transform: rotateY(calc(var(--i) * 45deg)) translateZ(400px);
-  -webkit-box-reflect: below 0px
-    linear-gradient(transparent, transparent, #0004);
+  left: 50%;
+  top: 40%;
+  z-index: 100;
+  width: 500px;
 }
-
-.box span img {
-  position: relative;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+#clicked1 .box {
+  width: 20vw;
+  max-width: 500px;
+  max-height: 200px;
 }
 </style>
