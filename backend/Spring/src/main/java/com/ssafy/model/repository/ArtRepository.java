86c532dbf453866bc293466031caa5a937813ba8
ydh2338 <ydh2338@naver.com>
2020-09-29 09:04:21 +0900

    :sparkles: Add Art Repository

---
+++ backend/Spring/src/main/java/com/ssafy/model/repository/ArtRepository.java
@@ -0,0 +1,26 @@
+package com.ssafy.model.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.model.dto.ArtDto;
+
+@Repository
+public interface ArtRepository extends JpaRepository<ArtDto, Integer> {
+
+	@Query(value = "select * from ssafy.art where art_title like :title limit :start ,16", nativeQuery = true)
+	public List<ArtDto> findArtByTitlelimit(@Param("title") String title, @Param("start") Integer start);
+
+	@Query(value = "select * from ssafy.art where art_artist = :artist limit :start ,16", nativeQuery = true)
+	public List<ArtDto> findArtByArtistlimit(@Param("artist") String artist, @Param("start") Integer start);
+
+	@Query(value = "select * from ssafy.art where art_museum = :museum limit :start ,16", nativeQuery = true)
+	public List<ArtDto> findArtByMuseumlimit(@Param("museum") String museum, @Param("start") Integer start);
+
+	@Query(value = "select * from ssafy.art where art_genre = :genre limit :start ,16", nativeQuery = true)
+	public List<ArtDto> findArtByGenrelimit(@Param("genre") String genre, @Param("start") Integer start);
+}
