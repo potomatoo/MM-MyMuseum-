4e60776acdc98bc3417394b9525a9ba8bcfb73d4
이연재 <duseh73@gmail.com>
2020-09-29 13:34:22 +0900

    :art: setting Axios

--- frontend/src/service/axios.service.ts
+++ frontend/src/service/axios.service.ts
@@ -14,6 +14,6 @@ export class Axios {
   });
 }
 
-Axios.instance.defaults.headers.common[
-  "Authorization"
-] = window.sessionStorage.getItem("jwt-token");
+// Axios.instance.defaults.headers.common[
+//   "Authorization"
+// ] = window.sessionStorage.getItem("jwt-token");
