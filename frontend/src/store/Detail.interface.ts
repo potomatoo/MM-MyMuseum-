a36095950406849d462be5b5683a9a27c80b42c4
이연재 <duseh73@gmail.com>
2020-09-29 16:56:00 +0900

    :sparkles: add SET_ART_LIST & FETCH_ART_LIST

--- frontend/src/store/Detail.interface.ts
+++ frontend/src/store/Detail.interface.ts
@@ -1,1 +1,18 @@
-export interface DetailModule {}
+export interface Art {
+  artNo: number;
+  artTitle: string;
+  artArtist: string;
+  artAge: string;
+  artSize: string;
+  artType: string;
+  artMehtod: string;
+  artMuseum: string;
+  artUrl: string;
+  artGenre: string;
+  artDescription: string;
+  art_color: string;
+}
+
+export interface DetailModule {
+  artList: Art[] | null;
+}
--- frontend/src/store/DetailModule.store.ts
+++ frontend/src/store/DetailModule.store.ts
@@ -1,14 +1,30 @@
 import { Module } from "vuex";
-import { Axios } from "@/service/axios.service";
-import { DetailModule } from "./Detail.interface";
 import { RootState } from "./index";
+import { Axios } from "@/service/axios.service";
+import { DetailModule, Art } from "./Detail.interface";
 
 const module: Module<DetailModule, RootState> = {
   namespaced: true,
-  state: {},
+  state: {
+    artList: null
+  },
   getters: {},
-  mutations: {},
-  actions: {}
+  mutations: {
+    SET_ART_LIST(state, artList: Art[]) {
+      state.artList = artList;
+    }
+  },
+  actions: {
+    FETCH_ART_LIST(
+      { commit },
+      { artist, start }: { artist: string; start: number }
+    ) {
+      Axios.instance
+        .get("/api/public/art/artist", { params: { artist, start } })
+        .then(({ data }) => commit("SET_ART_LIST", data.data))
+        .catch(err => console.error(err));
+    }
+  }
 };
 
 export default module;
