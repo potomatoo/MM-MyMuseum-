62c66205095044d7b882cb2ed6f5ddd42018b4f8
ydh2338 <ydh2338@naver.com>
2020-10-05 14:05:19 +0900

    :art: hotfix google redirect url

--- backend/Spring/src/main/java/com/ssafy/controller/GoogleLoginController.java
+++ backend/Spring/src/main/java/com/ssafy/controller/GoogleLoginController.java
@@ -69,7 +69,7 @@ public class GoogleLoginController {
 		parma.add("code", request.getParameter("code"));
 		parma.add("client_id", GoogleLoginConfig.GOOGLE_CLIENT_ID);
 		parma.add("client_secret", GoogleLoginConfig.GOOGLE_SECRIT_ID);
-		parma.add("redirect_uri", "http://i3b107.p.ssafy.io/api/public/google/redirect");
+		parma.add("redirect_uri", "http://j3b205.p.ssafy.io/api/public/google/redirect");
 		parma.add("grant_type", "authorization_code");
 
 		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
