627b5bc6d01136340ba47e78c924551d483b23b1
김준호 <tuvmxm@naver.com>
2020-10-05 21:00:36 +0900

    :art: Add color url

--- backend/Django/arts/urls.py
+++ backend/Django/arts/urls.py
@@ -4,5 +4,20 @@ from . import views
 app_name = 'arts'
 
 urlpatterns = [
+    # score
     path('user/', views.recommend_art),
+
+    # color
+    path('color/yellow/', views.recommend_yellow),
+    path('color/blue/', views.recommend_blue),
+    path('color/brown/', views.recommend_brown),
+    path('color/green/', views.recommend_green),
+    path('color/black/', views.recommend_black),
+    path('color/teal/', views.recommend_teal),
+    path('color/red/', views.recommend_red),
+    path('color/orange/', views.recommend_orange),
+    path('color/pink/', views.recommend_pink),
+    path('color/gray/', views.recommend_gray),
+    path('color/white/', views.recommend_white),
+    path('color/purple/', views.recommend_purple),
 ]
