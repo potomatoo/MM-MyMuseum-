b33069d745f0126b408065b72f9328c5816f7380
ydh2338 <ydh2338@naver.com>
2020-10-05 09:01:16 +0900

    :art: Add Favorite Service

---
+++ backend/Spring/src/main/java/com/ssafy/model/service/FavoriteService.java
@@ -0,0 +1,16 @@
+package com.ssafy.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.model.dto.FavoriteDto;
+
+@Service
+public interface FavoriteService {
+	public List<FavoriteDto> findFavorityById(String userId, int start);
+
+	public FavoriteDto saveFavorite(FavoriteDto favorite);
+
+	public void deleteFavorite(String userID, int artNo);
+}
