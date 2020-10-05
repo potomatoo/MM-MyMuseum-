0c7c60a758a2ba9dd210631593966abd5ac62f59
이연재 <duseh73@gmail.com>
2020-10-05 19:19:02 +0900

    :construction: change style to genre

---
+++ frontend/src/components/detail/DetailGenreImg.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-col class="artist-img align-self-center" v-if="artList">
+    <v-row style="height: 100%">
+      <v-col>
+        <transition name="fade">
+          <div
+            class="flip-container"
+            ontouchstart="this.classList.toggle('hover');"
+          >
+            <div class="flipper">
+              <img
+                v-if="artList[0].artUrl"
+                :src="artList[0].artUrl"
+                class="artist-img-front"
+              />
+              <img
+                v-if="artList[1].artUrl"
+                :src="artList[1].artUrl"
+                class="artist-img-back"
+              />
+            </div>
+          </div>
+        </transition>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+import { namespace } from "vuex-class";
+
+import { Art } from "../../store/Detail.interface";
+
+const DetailModule = namespace("DetailModule");
+
+@Component
+export default class DetailArtistImg extends Vue {
+  @DetailModule.State artList!: Art[] | null;
+  @Prop({ type: Boolean }) readonly show!: boolean;
+}
+</script>
+
+<style scoped>
+.flip-container {
+  perspective: 1000px;
+  border: 2px solid rgba(0, 0, 0, 0) !important;
+  margin: auto;
+}
+
+.artist-img-front {
+  z-index: 2;
+  transform: rotateY(0deg);
+  background-size: 100% 100%;
+  border-radius: 5px !important;
+  box-shadow: 0px 0px 10px 5px;
+}
+
+.flip-container,
+.artist-img-front {
+  width: 400px;
+  height: 500px;
+}
+</style>
