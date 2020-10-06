15c1ca39cbd08f5717fa3eb1024e38b836f0b5f1
김준호 <tuvmxm@naver.com>
2020-10-06 13:36:53 +0900

    :white_check_mark: Test token

--- backend/Django/arts/views.py
+++ backend/Django/arts/views.py
@@ -113,6 +113,7 @@ def recommend_art(request):
     # csv file load
     art_table = pd.read_csv('../../../recommend/art_score.csv')
     art_table.fillna('NaN', inplace=True)
+    print(request)
     if request.headers.get('Authorization'):
         # jwt token decoding
         token_str = request.headers.get('Authorization')
